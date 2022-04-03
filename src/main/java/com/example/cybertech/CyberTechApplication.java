df98b8c137b7a06e331d2434e19cf78dca00d5a5
Valentino_Garavanni <987bomber@gmail.com>
2022-04-03 14:11:22 +0300

    New website v1

---
+++ src/main/java/com/example/cybertech/CyberTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.cybertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cybertech/CyberTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cybertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
